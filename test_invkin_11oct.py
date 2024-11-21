b7a65d79ae8e5ff32c7bf0b4055ed1816a65fafd
Saniya <76146931+saniya2912@users.noreply.github.com>
2024-11-21 11:42:09 +0530

    Update test_invkin_11oct.py

--- test_invkin_11oct.py
+++ test_invkin_11oct.py
@@ -1,12 +1,14 @@
 import pickle
 import numpy as np
-from main_redundancy import GraspClass, TransMatrix, OnlyPosIK, convert_to_mujoco
+
+#write your own library with TransMatrix and OnlyPosIK classes as required 
+from main_redundancy import TransMatrix, OnlyPosIK
+
 from main import LeapNode
 import time
-# grasp = GraspClass()
+
 transmatrix = TransMatrix()
 leap=LeapNode()
-
 
 index_path='/home/saniya/LEAP/redundancy-leap/leap-mujoco/model/leap hand/redundancy/0_index.xml'
 thumb_path='/home/saniya/LEAP/redundancy-leap/leap-mujoco/model/leap hand/redundancy/0_thumb.xml'
@@ -32,8 +34,6 @@ palm_wrt_cam =  np.array([
     [0., 0., 0., 1.]
 ])
 
-mujoco_convert=convert_to_mujoco(palm_wrt_cam)
-
 
 b1 = np.array([[0],[-0.027],[0]])
 b2 = np.array([[0],[0.027],[0]])
@@ -49,17 +49,6 @@ def f(array):
     print('contact1',contactpos_1)
     print('contact2',contactpos_2)
 
-    # #convert to mujoco requirements
-    # v3=palm_wrt_cam[:3,3]+mujoco_convert.x_pbm_preal_c()-object_pose_cam[:3,3]
-    # x_obj_pbm=np.dot(np.dot(palm_wrt_cam[:3,:3].T,mujoco_convert.T_pbm_preal[:3,:3].T),-v3)
-
-    # x_obj_pbm=mujoco_convert.obj_pbm_from_preal(obj_pos)
-
-    
-
-    # print('obj_pos_mujoco',x_obj_pbm)
-
-    # obj_pos_mujoco=np.array([0,0,-0.1])
     
     qs1=pos_ik_index.calculate(contactpos_1,'contact_index')
     qs2=pos_ik_thumb.calculate(contactpos_2,'contact_thumb')
@@ -67,7 +56,7 @@ def f(array):
     qs2_real=qs2
     qs1_real[0],qs1_real[1]=qs1_real[1],qs1_real[0]
     qs=np.concatenate([qs1_real,np.zeros(8),qs2_real])
-    # print(qs)
+
     while True:
         leap.set_allegro(qs)
 
@@ -76,31 +65,22 @@ def load_and_compute(filename):
     # Load the array from a pickle file and compute f(array).
     # """
     try:
-        with open(filename, 'rb') as file:  # Changed the variable name to 'file' to avoid conflicts
-            print(f"Type of file: {type(file)}")  # Check if file is properly opened
+        with open(filename, 'rb') as file:  
+            print(f"Type of file: {type(file)}")  
             print("Attempting to load array from file...")
 
-            array = pickle.load(file)  # Ensure this is using the correct `pickle.load` method
-            #print("Loaded array:", array)
-
-            # Compute f(array)
+            array = pickle.load(file) 
+            
             result = f(np.array(array))
-            #print("Result of computation:", result)
+            
 
     except FileNotFoundError:
         print(f"File '{filename}' not found. Make sure the file exists.")
     except Exception as e:
         print(f"An error occurred: {e}")
-    # array=np.random.rand(4,4)
-    # f(np.array(array))
-    
+
 
 if __name__ == "__main__":
-    # Specify the filename of the saved array
     filename = '/tmp/received_array.pkl'
-    # while True:
-    #     load_and_compute(filename)
-    #     time.sleep(1)
-    
     load_and_compute(filename)
     
